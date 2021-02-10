8fe2d67cab32a015dffb43c0428905f74543f6a6
Dave Rigby <daver@couchbase.com>
2021-02-10 16:01:58 +0000

    Updating (commented-out) workaround
    If all associated classes are forward-declared with default visiblity the problem can be addressed.

--- boost_exception.h
+++ boost_exception.h
@@ -1,9 +1,7 @@
 #define PUBLIC_API __attribute__((visibility("default")))
 
-// I expected that adding external (default) visibility to a
-// forward-decl of boost::error_info & error_info_base would fix the
-// problem, but it doesn't - uncommenting this still gives the same
-// error.
+// Forward-declaring _all_ of the following classes with extenral
+// visibilty addresses the issue:
 #if 0
 namespace boost {
 template <class Tag, class T>
@@ -13,11 +11,17 @@ namespace boost { namespace exception_detail {
 class PUBLIC_API error_info_base;
 };
 };
+namespace boost { namespace stacktrace {
+class PUBLIC_API frame;
+template <class Allocator> class PUBLIC_API basic_stacktrace;
+};
+};
 #endif
 
 #include <boost/stacktrace.hpp>
 #include <boost/exception/all.hpp>
 
+struct PUBLIC_API tag_stacktrace;
 using traced = boost::error_info<struct tag_stacktrace, boost::stacktrace::stacktrace>;
 
 extern void foo(int);
