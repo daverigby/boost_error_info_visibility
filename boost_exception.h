645f8bbe0145c4fbe23d2f751953d9cd94ef566a
Dave Rigby <daver@couchbase.com>
2021-02-10 15:17:14 +0000

    Try fwd-decl of error_info_base
    Doesn't help.

--- boost_exception.h
+++ boost_exception.h
@@ -1,12 +1,17 @@
 #define PUBLIC_API __attribute__((visibility("default")))
 
 // I expected that adding external (default) visibility to a
-// forward-decl of boost::error_info would fix the problem, but it
-// doesn't - uncommenting this still gives the same error.
+// forward-decl of boost::error_info & error_info_base would fix the
+// problem, but it doesn't - uncommenting this still gives the same
+// error.
 #if 0
 namespace boost {
 template <class Tag, class T>
 class PUBLIC_API error_info;
+};
+namespace boost { namespace exception_detail {
+class PUBLIC_API error_info_base;
+};
 };
 #endif
 
